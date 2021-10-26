622df83a898da51c602dba744a8fd1e59c29c462
AyeshaShaharyar <ayesha2598@gmail.com>
2021-10-26 03:14:36 +0000

    use of functions

---
+++ odd.js
@@ -0,0 +1,10 @@
+function isOdd(num){
+  if (num % 2 === 0){
+    even = num + " is odd: false" 
+    return even;
+  } else {
+    even = num + " is odd: true" 
+  }
+  return even;
+}
+console.log (isOdd(5));
